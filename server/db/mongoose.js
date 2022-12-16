d44d9d9b2389481e99420be5c36da271b484a541
ashgolan <ashgolan@hotmail.com>
2022-12-16 22:44:37 +0200

    mongoose.set('strictQuery', false)

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
-
+mongoose.set("strictQuery", false);
 mongoose.connect(URL, (err, mongoDbInstance) => {
   if (err) throw Error("MongooDb connection Error ! -- " + err);
 
