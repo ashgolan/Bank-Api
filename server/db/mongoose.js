91d81adfc967e21c1484fc86b234ebba3249eaee
ashgolan <ashgolan@hotmail.com>
2022-12-17 20:53:33 +0200

    ..

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,30 +1,30 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
-// mongoose.set("strictQuery", false);
-// mongoose.connect(URL, async (err, mongoDbInstance) => {
-//   try {
-//     if (err) throw Error((await "MongooDb connection Error ! -- ") + err);
-//   } catch (e) {
-//     console.log("error");
-//   }
-//   // const { host, port, name } = mongoDbInstance;
-//   // console.log({ host, port, name });
-// });
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
 const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.set("strictQuery", false);
-mongoose
-  .connect(URL)
-  .then(() => {
-    console.log("connected to mongoDB");
-  })
-  .catch((e) => {
-    console.log("MongooDb connection Error" + e);
-  });
+mongoose.connect(URL, async (err, mongoDbInstance) => {
+  try {
+    if (err) throw Error((await "MongooDb connection Error ! -- ") + err);
+  } catch (e) {
+    console.log("error");
+  }
+  // const { host, port, name } = mongoDbInstance;
+  // console.log({ host, port, name });
+});
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
+// // mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(URL)
+//   .then(() => {
+//     console.log("connected to mongoDB");
+//   })
+//   .catch((e) => {
+//     console.log("MongooDb connection Error" + e);
+//   });
