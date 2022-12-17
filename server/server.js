40f7712efafaf20216499d079365f3cd99bff48f
ashgolan <ashgolan@hotmail.com>
2022-12-17 21:27:36 +0200

    ....

--- server/server.js
+++ server/server.js
@@ -8,11 +8,10 @@ import { index } from "./utils/index.js";
 import "./db/mongoose.js";
 import cors from "cors";
 const PORT = process.env.PORT || 3001; // changed
-// const PORT = process.env.PORT || 3001; // changed
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true }));
 
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
