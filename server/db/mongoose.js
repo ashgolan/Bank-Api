98dadc9bcee369da75d61e75c2040ed290652178
ashgolan <ashgolan@hotmail.com>
2022-12-17 20:56:17 +0200

    ..

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,11 +5,8 @@ dotenv.config();
 const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.set("strictQuery", false);
 mongoose.connect(URL, (err, mongoDbInstance) => {
-  try {
-    if (err) throw Error("MongooDb connection Error ! -- " + err);
-  } catch (e) {
-    console.log("error");
-  }
+  if (err) throw Error("MongooDb connection Error ! -- " + err);
+
   // const { host, port, name } = mongoDbInstance;
   // console.log({ host, port, name });
 });
