e888a0472a5dedd474998d0f10005a59dff47923
ashgolan <ashgolan@hotmail.com>
2022-12-17 21:53:52 +0200

    ....

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -16,7 +16,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 mongoose
   .connect(URL)
   .then(() => {
